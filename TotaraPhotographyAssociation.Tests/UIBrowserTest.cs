03aad6f404f3cdb95f1659ce74ea68ecf330855c
Francis-Underwood <thatindividual.zhang@gmail.com>
2016-10-31 00:25:19 +1300

    selenium test

---
+++ TotaraPhotographyAssociation.Tests/UIBrowserTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+
+namespace TotaraPhotographyAssociation.Tests
+{
+    [TestClass]
+    public class UIBrowserTest
+    {
+        static IWebDriver driverChrome;
+        static IWebDriver driverIE;
+
+        public static TestContext testContext { get; set; }
+
+        [AssemblyInitialize]
+        public static void SetUp(TestContext tc)
+        {
+            testContext = tc;
+            /*
+            driverChrome = new ChromeDriver();
+            System.Threading.Thread.Sleep(6000);
+            driverChrome.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            */
+
+            IWebDriver driverIE = new InternetExplorerDriver(@"D:\workcomplex\SeleniumDrivers");
+            System.Threading.Thread.Sleep(6000);
+            driverIE.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+        }
+/*
+        [AssemblyCleanup]
+        public static void CleanUp()
+        {
+            driverChrome.Quit();
+        }
+        */
+
+        [TestMethod]
+        public void TestLogin()
+        {
+            //IWebDriver driver = new ChromeDriver(@"D:\workcomplex\SeleniumDrivers");
+            /*
+            IWebDriver driver = new InternetExplorerDriver(@"D:\workcomplex\SeleniumDrivers");
+            System.Threading.Thread.Sleep(6000);
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl("http://localhost:42439/");
+            */
+            //driverChrome.Navigate().GoToUrl("http://localhost:42439/");
+            driverIE.Navigate().GoToUrl("http://localhost:42439/");
+
+        }
+
+
+    }
+
+
+}
